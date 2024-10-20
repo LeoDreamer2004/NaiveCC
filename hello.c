f5f7ba921520afab4096f7959186a0b8b0d9ddd4
原梓轩 <2200010825@stu.pku.edu.cn>
2024-10-20 16:44:08 +0800

    lvl6 fix++++

--- hello.c
+++ hello.c
@@ -1,4 +1,14 @@
 int main() {
-  if (0) return 1;
-  else return 2;
+  int a = 10;
+  if (a > 1)
+    if (a > 2)
+      if (a < 3)
+        return a;
+      else
+        if (a > 4)
+          if (a < 5)
+            return a + 1;
+          else
+            return a + 2;
+  return -1;
 }
