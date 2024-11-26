540988f2783da1052fcaa1b12828d117ad795e62
LeoDreamer <2200010825@stu.pku.edu.cn>
2024-11-26 20:05:19 +0800

    add a useless opt

--- src/frontend/opt/mod.rs
+++ src/frontend/opt/mod.rs
@@ -3,9 +3,11 @@ mod ci;
 mod cse;
 mod dbe;
 mod dce;
+mod uce;
 
 pub use bfs::BlockFlowSimplifier;
 pub use ci::ConstantsInline;
 pub use cse::CommonSubexpression;
 pub use dbe::DeadBlockElimination;
 pub use dce::DeadCodeElimination;
+pub use uce::UnreadCodeElimination;
